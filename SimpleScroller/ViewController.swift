65965a0c5eec651d8e0940a9695ef8913475ad5d
Abhijith PM <pmabi1993@outlook.com>
2025-02-22 14:16:12 +0800

    Initial Commit

---
+++ SimpleScroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleScroller
+//
+//  Created by Abhijith Pm on 22/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
